cd29953ef40b1d1ae36149605523983231c02e87
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-04-17 19:00:04 -0300

    useState

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,20 +1,43 @@
-
+import {useState} from 'react'
+import styles from './Forms.module.css'
 function Form() {
     let nomesCadastrados = {}
     function cadastrarUsuario(e) {
         e.preventDefault()
-        let nome = document.querySelector('#nome')
-        console.log(`${nome.value} Cadastrado`)
+        console.log(`O usuario:${name} se cadastrou com a senha:${senha}`)
+        console.log('Cadastrou o usuário')
     }
+
+    const [name, setName] = useState('Vinicius')
+    const [senha, setSenha] = useState('joaozinh1123')
+
     return(
         <div>
             <h1>Meu Cadastro</h1>
-            <form onSubmit={cadastrarUsuario}>
-                <div>
-                    <input type="text" placeholder="Digite seu nome" id="nome"></input>
+            <form onSubmit={cadastrarUsuario} className={styles.boxLogin} >
+                <div className={styles.boxName}>
+                    <label htmlFor="name">Nome: </label>
+                    <input 
+                        type="text"
+                        placeholder="Digite seu nome" 
+                        autoComplete='off'
+                        id="name"
+                        onChange={(e) => setName(e
+                        .target.value)}
+                     >
+                     </input>
+                </div>
+                <div className={styles.boxSenha}>
+                    <label htmlFor="senha">Senha: </label>
+                    <input 
+                        type="password" 
+                        placeholder="Digite sua senha" 
+                        id="senha"
+                        onChange={(e) => setSenha(e.target.value)}
+                    ></input>
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" ></input>
+                    <input type="submit" value="Cadastrar"></input>
                 </div>
             </form>
         </div>
--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 
 function Item({marca, ano_lancamento}) {
+    
     return(
         <>
             <li>{marca} - {ano_lancamento}</li>
